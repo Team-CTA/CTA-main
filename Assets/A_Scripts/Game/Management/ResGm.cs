0f4ffa20f94c608452fd7687a1a6530315330659
PDLAKE-1325 <163096820+PDLAKE-1325@users.noreply.github.com>
2024-12-27 09:09:22 +0900

    결과창 효과 개선

---
+++ Assets/A_Scripts/Game/Management/ResGm.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
+
+public class ResGm : MonoBehaviour
+{
+    [SerializeField] Text myScoreText;
+    [SerializeField] Text enScoreText;
+    [SerializeField] Text resultText;
+    [SerializeField] Text infoText;
+    [SerializeField] GameObject resultPannel;
+    int myScore, eneScore, myGroundScore, eneGroundScore;
+
+    void Start()
+    {
+        myScore = PlayerPrefs.GetInt("MyScore");
+        eneScore = PlayerPrefs.GetInt("EnemyScore");
+        myGroundScore = PlayerPrefs.GetInt("MyScoreG");
+        eneGroundScore = PlayerPrefs.GetInt("EnemyScoreG");
+        StartCoroutine(setText());
+    }
+    IEnumerator setText()
+    {
+        infoText.text = "클리어한 미니게임 포인트";
+        int sI = 0, sE = 0;
+        for (int i = 0; i < myScore; i++)
+        {
+            yield return new WaitForSeconds(0.05f);
+            sI++;
+            myScoreText.text = $"{sI}";
+        }
+        for (int i = 0; i < eneScore; i++)
+        {
+            yield return new WaitForSeconds(0.05f);
+            sE++;
+            enScoreText.text = $"{sE}";
+        }
+        yield return new WaitForSeconds(1f);
+        infoText.text = "점령 구역 포인트";
+        for (int i = 0; i < myGroundScore; i++)
+        {
+            yield return new WaitForSeconds(0.05f);
+            sI++;
+            myScoreText.text = $"{sI}";
+        }
+        for (int i = 0; i < eneGroundScore; i++)
+        {
+            yield return new WaitForSeconds(0.05f);
+            sE++;
+            enScoreText.text = $"{sE}";
+        }
+        infoText.text = "결산 종료";
+        yield return new WaitForSeconds(3f);
+        int resme = myScore + myGroundScore;
+        int resenemy = eneScore + eneGroundScore;
+        if (resme > resenemy)
+        {
+            resultText.text = "승리";
+        }
+        else if (resme == resenemy)
+        {
+            resultText.text = "무승부";
+        }
+        else if (resme < resenemy)
+        {
+            resultText.text = "패배";
+        }
+        resultPannel.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        PhotonNetwork.LeaveRoom();
+        SceneManager.LoadScene("Main");
+    }
+}
