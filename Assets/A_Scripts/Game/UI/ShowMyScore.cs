1b7c64dce5a9e58b50f5f8988cdabacd92f25dfd
minjae0402 <kangjack0402@gmail.com>
2024-12-29 20:23:23 +0900

    Add : User Stat

--- Assets/A_Scripts/Game/UI/ShowMyScore.cs
+++ Assets/A_Scripts/Game/UI/ShowMyScore.cs
@@ -4,9 +4,20 @@ using UnityEngine.UI;
 public class ShowMyScore : MonoBehaviour
 {
     public Text MyScore;
+    public Text MyWIN;
+    public Text MyLOSSES;
+    public Text MyDRAW;
+    public Text MyPLAY;
+    public Text MyWINRATE;
     void Start()
     {
         MyScore.text = PlayerPrefs.GetInt("UserScore").ToString();
+
+        MyWIN.text = StatManager.Instance.GetUserWins().ToString();
+        MyLOSSES.text = StatManager.Instance.GetUserLosses().ToString();
+        MyDRAW.text = StatManager.Instance.GetUserDraws().ToString();
+        MyPLAY.text = StatManager.Instance.GetTotalGames().ToString();
+        MyWINRATE.text = StatManager.Instance.GetUserWinRate().ToString() + "%";
 
 
         Debug.Log(PlayerPrefs.GetInt("UserRank")); //강민재 : 유저 랭크 가져오는 법
