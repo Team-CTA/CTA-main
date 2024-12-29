b22909fba52e9d63baf848f8abba14cbc53030bf
minjae0402 <kangjack0402@gmail.com>
2024-12-29 19:46:45 +0900

    Fix : pos

--- Assets/A_Scripts/Game/UI/ShowMyScore.cs
+++ Assets/A_Scripts/Game/UI/ShowMyScore.cs
@@ -7,5 +7,15 @@ public class ShowMyScore : MonoBehaviour
     void Start()
     {
         MyScore.text = PlayerPrefs.GetInt("UserScore").ToString();
+
+
+        Debug.Log(PlayerPrefs.GetInt("UserRank")); //강민재 : 유저 랭크 가져오는 법
+        Debug.Log(PlayerPrefs.GetInt("UserScore")); //강민재 : 유저 스코어 가져오는 법
+
+        Debug.Log(StatManager.Instance.GetTotalGames()); //강민재 : 총 게임 플레이수 가져오는 법
+        Debug.Log(StatManager.Instance.GetUserWins()); //강민재 : 총 이긴 횟수 가져오는 법
+        Debug.Log(StatManager.Instance.GetUserLosses()); //강민재 : 총 진 횟수 가져오는 법
+        Debug.Log(StatManager.Instance.GetUserDraws()); //강민재 : 총 무승부한 횟수 가져오는 법
+        Debug.Log(StatManager.Instance.GetUserWinRate()); //강민재 : 승률 가져오는 법
     }
 }
